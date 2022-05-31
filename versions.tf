e173f384e154a537c982ca967bd07275aba6fe05
Jordan Olin (OLINSolutions) <jordan@olinsolutions.com>
2022-05-30 20:15:57 -0600

    Remove cloud block

--- versions.tf
+++ versions.tf
@@ -1,12 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "hashicorp-learn"
+    organization = "OLINSolutions"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
+*/
 
   required_providers {
     aws = {
